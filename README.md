8baa65cff3f3b60398daacc56fd59f9c3cddfa48
TegarUtomo <mochamad.tegar.utomo@basajans.com>
2018-05-08 10:20:47 +0700

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+﻿## Getting started with C#
+
+
+Software you must installed:
+1. Microsoft Visual Studio Update 3 [download here](https://my.visualstudio.com/Downloads?q=visual%20studio%202015&wt.mc_id=o~msft~vscom~older-downloads)
+
+You can start in your locally, follow this steps :
+1. ``` git clone https://github.com/aditiadj/WebApp ``` in terminal
+2. ``` cd WebApp ```
+3. ``` Start Debugging (F5) in your browser (Automatically)```
+
+#### Tech Stack
+1. C#
+
+Thanks to [this article](https://docs.microsoft.com/en-us/aspnet/web-api/overview/getting-started-with-aspnet-web-api/tutorial-your-first-web-api)
---
+++ WebApp/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Coto Makassar", Category = "Groceries", Price = 2.5M },
+            new Product { Id = 2, Name = "Panggal", Category = "Toys", Price = 1.5M },
+            new Product { Id = 3, Name = "Shovel", Category = "Hardware", Price = 15 }
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
